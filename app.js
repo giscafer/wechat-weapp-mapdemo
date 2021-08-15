29c1a07d85c121be83729e47908480955e00f04a
Yueqian8Longma <49428274+Yueqian8Longma@users.noreply.github.com>
2021-08-15 10:51:55 +0800

    commit -m "test"

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ App({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
-              that.globalData.userInfo = res.userInfo;
-              typeof cb == "function" && cb(that.globalData.userInfo)
             }
           })
         }
